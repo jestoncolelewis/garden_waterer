d0336b30f21b6d9b0a59f66aa51e1c4f81f2eba1
Jeston Lewis <jeston.lewis@protonmail.com>
2023-08-26 23:39:21 -0600

    pseudocode of process

--- garden_waterer.ino
+++ garden_waterer.ino
@@ -1,7 +1,15 @@
 void setup() {
-
+    // setup input pins for moisture detector
+    // setup network connection
+    // self identify bed
+    // get historical data
+    // get weather data
 }
 
 void loop() {
-    
+    // check moisture levels
+    // if moisture levels are below desired level start watering
+    // else delay 8 hours
+    // check weather for next 2 hours
+    // if rain expectation is above some percent wait??
 }
