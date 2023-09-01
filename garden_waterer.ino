f3e11f8973df595ed5ba134251bb46b086bf542b
Jeston <jeston.lewis@protonmail.com>
2023-09-01 10:31:10 -0600

    added logic for pump operation

--- garden_waterer.ino
+++ garden_waterer.ino
@@ -1,8 +1,11 @@
 int analogPin = A0; // input pin for moisture detector
 int moistureLevel = 0; // initialize moisture level
+int outputPin = 3;
 
 void setup() {
     Serial.begin(9600);
+    pinMode(analogPin, INPUT);
+    pinMode(outputPin, OUTPUT);
 }
 
 void loop() {
@@ -11,9 +14,11 @@ void loop() {
     Serial.println();
     delay(3000);
     
-    // while (moistureLevel > 480) { // while moisture levels are below desired level
-      // start pump
-    // }
+    if (moistureLevel > 480) { // while moisture levels are below desired level
+      digitalWrite(outputPin, HIGH); // start pump
+      delay(1000 * 60 * 5); // ms * sec * min
+      digitalWrite(outputPin, LOW); //stop pump
+    }
     // delay 8 hours
-    // delay(1000 * 60 * 60 * 8); // ms * sec * min * hrs
+    delay(1000 * 60 * 60 * 8); // ms * sec * min * hrs
 }
