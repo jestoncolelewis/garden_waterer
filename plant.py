e78c0e86ce9a61e5e7c9b3e1ee01038717f79798
Jeston Lews <jeston@jestonlewiscreative.com>
2023-11-04 20:18:31 -0500

    updated variables names

--- plant.py
+++ plant.py
@@ -4,28 +4,28 @@ import time
 class Plant:
     def __init__(self, name: str, dl: int) -> None:
         self.name = name
-        self.drynessLimit = dl
-        self.moistureLevel: int = 0
+        self.dryness_limit = dl
+        self.moisture_level: int = 0
     
     # getters/setters
     def get_name(self) -> str:
         return self.name
 
     def get_moisture_level(self) -> int:
-        return self.moistureLevel
+        return self.moisture_level
 
     def get_dryness_limit(self) -> int:
-        return self.drynessLimit
+        return self.dryness_limit
     
     def set_moisture_level(self, ml) -> None:
-        self.moistureLevel = ml
+        self.moisture_level = ml
 
     def set_dryness_limit(self, dl) -> None:
-        self.drynessLimit = dl
+        self.dryness_limit = dl
 
     # methods
     def water_if_needed(self) -> None:
-        if self.moistureLevel > self.drynessLimit:
+        if self.moisture_level > self.dryness_limit:
             print("Starting watering")
             # send start command to arduino
             time.sleep(1)
