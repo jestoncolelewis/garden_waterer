34a2b6a690dfee5efa89ff3f275fc965dd32eb1a
Jeston Lews <jeston@jestonlewiscreative.com>
2023-11-04 20:44:57 -0500

    started defining struct

---
+++ main.c
@@ -0,0 +1,5 @@
+#include "plant.h"
+
+int main() {
+    Plant jade;
+}
---
+++ plant.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef struct {
+    char* name;
+    int moistureLevel;
+    int drynessLimit;
+} Plant;
+
+void waterIfNeeded() {
+    if () {
+
+    }
+}
