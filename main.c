52752b5716df117403c8cdbe64e9bb124a88e23a
Jeston Lews <jeston@jestonlewiscreative.com>
2023-11-07 19:27:35 -0700

    stripping down to rebuild

--- main.c
+++ main.c
@@ -3,28 +3,5 @@
 
 
 int main(void) {
-    char *portname = "/dev/ttyUSB1";
-    int fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
-
-    Plant jade;
-    jade.name = "Jade";
-    jade.drynessLimit = 480;
-
-    if (fd < 0) {
-        printf("error %d opening %s: %s", errno, portname, strerror (errno));
-        return -1;
-    }
-
-    set_interface_attribs (fd, B115200, 0);
-    set_blocking (fd, 0);
-
-    write(fd, "hello!\n", 7);
-
-    usleep((7 + 25) * 100);
-    char buf [100];
-    int n = read(fd, buf, sizeof buf);
-
-    jade.moistureLevel = n;
-
     return 0;
 }
