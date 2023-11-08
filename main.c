b7473709505331269cc56297fd142fb88425a105
Jeston Lews <jeston@jestonlewiscreative.com>
2023-11-08 09:24:55 -0700

    rebuilding serial_tool

--- main.c
+++ main.c
@@ -3,5 +3,13 @@
 
 
 int main(void) {
+    int fd = open_port();
+
+    ssize_t n = write(fd, "ATZ\r", 4);
+    if (n < 0)
+        fputs("write() of 4 bytes failed", stderr);
+
+    close(fd);
+
     return 0;
 }
--- serial_tool.h
+++ serial_tool.h
@@ -1,6 +1,21 @@
-#include <errno.h>
-#include <fcntl.h>
+#include <stdio.h>
 #include <string.h>
-#include <termios.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
 
+#define PORT "/dev/ttyUSB"
+
+int open_port (void) {
+    int fd;
+
+    fd = open (PORT, O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd == -1) {
+        perror ("open_port: Unable to open port /dev/ttyUSB");
+    } else {
+        fcntl (fd, F_SETFL, 0);
+    }
+
+    return (fd);
+}
